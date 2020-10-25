332b0f61a82e150e37b8783669bc449c6dc38511
Chris <pcc386@nyu.edu>
2020-10-25 16:01:35 +0400

    added new image and maybe fix for gif display

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function switchGif(){
 	var endGif = document.getElementById("offPanel");
 	var endText = document.getElementById("turnoff");
 	if(switchStatus){
-		endGif.src="./Images/panel8.gif";
+		endGif.src="./Images/panel8.GIF";
 		endText.style.opacity=0;
 		switchStatus=false;
 	}
