7ba35a2d710ccd39474386beabc5436c1dba0999
Chris <pcc386@nyu.edu>
2020-10-25 15:26:50 +0400

    more parallax with onclick event for last paenl

---
+++ script.js
@@ -0,0 +1,10 @@
+let switchStatus=true;
+function switchGif(){
+	var endGif = document.getElementById("offPanel");
+	if(switchStatus){
+		endGif.src="./Images/panel8.gif";
+		switchStatus=false;
+	}
+
+
+}
