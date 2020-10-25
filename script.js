1edcdef18d40c96824a7a83bb24bee9d40e6e8e5
Chris <pcc386@nyu.edu>
2020-10-25 15:40:38 +0400

    changed text to disappear oso

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let switchStatus=true;
 function switchGif(){
 	var endGif = document.getElementById("offPanel");
+	var endText = document.getElementById("turnoff");
 	if(switchStatus){
 		endGif.src="./Images/panel8.gif";
+		endText.style.opacity=0;
 		switchStatus=false;
 	}
 
